b02b4c3216ea37e8d3d0bbb60eabb0854a6b8e54
Gunjesh Basnet <gunjesh.basnet550208@gmail.com>
2023-11-08 12:48:20 +0545

    localStorage

--- script.js
+++ script.js
@@ -1,80 +1,33 @@
-const form = document.querySelector("#todo-form");
-const input = document.querySelector("#todo-input");
-const ul = document.querySelector("#todo-list");
+import { Todo, TodoList } from "./list.js";
 
-class Todo {
-  constructor(task) {
-    this.id = Date.now();
-    this.task = task;
-    this.completed = false;
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const input = document.querySelector("input");
+  const ul = document.getElementById("todo-list");
+  const removeTasksButton = document.querySelector(".remove-task-button");
+
+  const TODO_LIST = new TodoList();
+  TODO_LIST.initializeWithCachedTodos();
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (input.value.trim() === "") {
+      return;
+    }
+
+    const newId = Date.now();
+    const newTask = new Todo(input.value, newId);
+
+    TODO_LIST.addTodo(newTask);
+
+    input.value = "";
+  });
+
+  removeTasksButton.addEventListener("click", removeAllTasks);
+
+  function removeAllTasks() {
+    TODO_LIST.items = [];
+    TODO_LIST.saveTodoToLocalStorage();
+    TODO_LIST.renderToPage();
   }
-}
-
-class TodoList {
-  constructor() {
-    this.items = [];
-  }
-
-  initializeWithCachedTodos(todos) {
-    this.items = todos;
-  }
-
-  addTodo(todo) {
-    this.items.push(todo);
-  }
-
-  toggleTodoStatus(todoId) {
-    const newTodoList = this.items.map((item) => {
-      if (item.id === todoId) {
-        return {
-          ...item,
-          completed: !item.completed,
-        };
-      }
-      return item;
-    });
-
-    this.items = newTodoList;
-  }
-
-  renderToPage() {
-    ul.innerHTML = "";
-
-    this.items.forEach((item) => {
-      const li = document.createElement("li");
-
-      li.innerText = item.task;
-
-      const button = document.createElement("button");
-      button.innerText = item.completed ? "COMPLETED" : "TO-DO";
-      button.id = item.id;
-      button.classList.add("toggle-btn");
-      if (item.completed) {
-        button.classList.add("complete");
-      }
-      button.addEventListener("click", handleStatusChange);
-
-      li.appendChild(button);
-      ul.appendChild(li);
-    });
-  }
-}
-
-const TODO_LIST = new TodoList();
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (input.value === "") return;
-
-  const newTask = new Todo(input.value);
-
-  TODO_LIST.addTodo(newTask);
-  TODO_LIST.renderToPage();
-
-  input.value = "";
 });
-
-function handleStatusChange(event) {
-  TODO_LIST.toggleTodoStatus(+event.target.id);
-  TODO_LIST.renderToPage();
-}
